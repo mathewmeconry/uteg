5dccb13fc5b98222895a46de4cc3606a3d89b9cb
Mathias Scherer <mathias.scherer@adwebster.com>
2016-02-08 17:53:08 +0100

    build list for judges

--- src/uteg/Controller/JudgeController.php
+++ src/uteg/Controller/JudgeController.php
@@ -10,14 +10,30 @@ use Symfony\Component\HttpFoundation\Response;
 use uteg\Entity\Club;
 use uteg\Form\Type\ClubType;
 
-class ClubsController extends DefaultController
+class JudgeController extends DefaultController
 {
     /**
      * @Route("/judging", name="judging")
      * @Method("GET")
      */
-    public function judgingAction(Request $request, $compid)
+    public function judgingAction(Request $request)
     {
+        $this->get('acl_competition')->isGrantedUrl('IS_AUTHENTICATED_FULLY', false);
 
+        $comps = array();
+
+        $user = $this->getUser();
+        $authorizationChecker = $this->get('security.authorization_checker');
+
+        foreach ($user->getJ2cs() as $j2c) {
+            $request->getSession()->set('comp', $j2c->getCompetition()->getId());
+            if ($authorizationChecker->isGranted('VIEW', $j2c->getCompetition())) {
+                $j2cs[] = $j2c;
+            }
+        }
+
+        return $this->render('judging.html.twig', array(
+            "j2cs" => $j2cs
+        ));
     }
 }
